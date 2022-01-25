2f4d31f22fec08f34bb69df3b65882ef9dea9d33
Foxaar <95308315+Foxaar@users.noreply.github.com>
2022-01-25 09:52:24 +0000

    all logic done

--- main.cs
+++ main.cs
@@ -6,5 +6,20 @@ Console.WriteLine("Исходный массив");
     Console.WriteLine("hello 2 world :-)");
     
     string [] array = {"hello","2","world",":-)"};
+
+    Console.WriteLine("Выборка");
+      for (int i=0;i<=array.Length;i++) {
+         
+        string a = array[i];
+        if (a.Length <= 3) {
+
+        string []result = (string [])array.Clone();
+
+        string b = result[i];
+        
+
+        Console.Write(b + " ");
+        }
+      }
     }
 }
