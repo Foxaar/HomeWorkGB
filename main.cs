4d75ca6c5f3759a62c08a15b53493e717d660b48
Foxaar <95308315+Foxaar@users.noreply.github.com>
2022-01-25 11:11:31 +0000

    changed code structure

--- main.cs
+++ main.cs
@@ -2,27 +2,25 @@ using System;
 
 class Program {
     static void Main(string[] args) {
-Console.WriteLine("Исходный массив");
-    Console.WriteLine("hello 2 world :-)");
+      Console.WriteLine("Исходный массив");
+      Console.WriteLine("hello 2 world :-)");
     
-    string [] array = {"hello","2","world",":-)"};
+      string [] array = {"hello","2","world",":-)"};
 
-    Console.WriteLine("Выборка");
-      for (int i=0;i<=array.Length-1;i++) {
+      Console.WriteLine("Выборка");
+        for (int i=0;i<=array.Length-1;i++) {
          
-        string arrStart = array[i];
-        if (arrStart.Length <= 3) {
+          string arrStart = array[i];
+          
+            if (arrStart.Length <= 3) {
 
-        string []result = (string [])array.Clone();
+              string []result = (string [])array.Clone();
 
-        string arrResult = result[i];
-        
+              string arrResult = result[i];
 
-        Console.Write(arrResult + " ");
-        
+                Console.Write(arrResult + " ");
+            }
         }
-        
-      }
     Console.WriteLine("");
     Console.WriteLine("Конец программы");
     }
